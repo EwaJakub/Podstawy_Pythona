a6049dbb7cdb85ace63b8abdf4c74134be60d615
EwaJakub <ewa.jak@onet.pl>
2023-01-16 18:35:36 +0100

    zmienne, zadanie_01

---
+++ 01_Dzien_1/02_Zmienne/zadanie_1.py
@@ -0,0 +1,11 @@
+temp = 15
+temp_2 = '15'
+
+print(temp * temp_2)
+print(type(temp))  #int type
+print(type(temp_2))  # str type
+
+print(temp + temp_2)
+print(temp * temp_2)
+print(temp / temp_2)
+# unsupported operand type
