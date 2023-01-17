f26f0d90bd62f5c6873a2d41a8dd2629a7398e32
EwaJakub <ewa.jak@onet.pl>
2023-01-17 19:15:46 +0100

    Funkcje, zadanie_01

--- 01_Dzien_1/04_Funkcje/01_Zadanie_1/exercise_1.py
+++ 01_Dzien_1/04_Funkcje/01_Zadanie_1/exercise_1.py
@@ -1,7 +1,16 @@
 # write the function from exercise 1 here
+def square(num):
+    return num ** 2
+
+def square_print(num):
+    print(num ** 2)
 
 # the lines below will test your function:
 print("2 squared is:", square(2))  # should be 4
 print("16^2=", square(16))  # should be 256
 print("256 to the 2nd power =", square(256))  # should be 65536
- 
+
+a = square(10) + 10
+print(a)
+b = square_print(10) + 10 # returned value is None, it can't be a sum with int
+print(b)
