1abc362d1112fd0f376b0d33fbb818a2bc4e1b68
EwaJakub <ewa.jak@onet.pl>
2023-01-17 19:31:29 +0100

    Funkcje, zadanie_04

--- 01_Dzien_1/04_Funkcje/04_Zadanie_4/exercise_4.py
+++ 01_Dzien_1/04_Funkcje/04_Zadanie_4/exercise_4.py
@@ -1,5 +1,7 @@
 # write the function from exercise 4 here
+def convert_to_usd(zlotys):
+    return round(zlotys / 3.85, 2)
 
 # the lines below will test your function:
 print("385EUR = ", convert_to_usd(385), "USD")
-print("100EUR = ", convert_to_usd1(100), "USD")
+print("100EUR = ", convert_to_usd(100), "USD")
