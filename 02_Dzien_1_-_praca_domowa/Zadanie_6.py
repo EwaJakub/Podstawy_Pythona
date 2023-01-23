4157e47014ed406b2b612b10ab7bcd9754a0d505
EwaJakub <ewa.jak@onet.pl>
2023-01-23 21:27:06 +0100

    Praca_domowa; dzien_1; Zadanie_6; is_divided_by_4

---
+++ 02_Dzien_1_-_praca_domowa/Zadanie_6.py
@@ -0,0 +1,8 @@
+def is_divisible_by_4(number):
+    return number % 4 == 0
+
+print(is_divisible_by_4(4))  # True
+print(is_divisible_by_4(5))  # False
+print(is_divisible_by_4(20))  # True
+print(is_divisible_by_4(31))  # False
+print(is_divisible_by_4(100))  # True
