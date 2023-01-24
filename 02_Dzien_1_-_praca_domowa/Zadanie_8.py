c4a47a9e4a5263baf4d6ff6d85bb662c2f6bf9f9
EwaJakub <ewa.jak@onet.pl>
2023-01-24 20:54:29 +0100

    Praca_domowa; dzien_1; Zadanie_8; ciąg_fibonacciego

--- 02_Dzien_1_-_praca_domowa/Zadanie_8.py
+++ 02_Dzien_1_-_praca_domowa/Zadanie_8.py
@@ -5,6 +5,13 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
-print(fib(4))  # 3
+def fib_2(n):
+    result = [1, 1]
+    for i in range(n-2):
+        result.append(result[i] + result[(i + 1)])
+    return result[-1]
+
+print(fib(3))  # 2
+print(fib_2(4))  # 3
 print(fib(8))  # 21
-print(fib(16))  # 987
+print(fib_2(16))  # 987
