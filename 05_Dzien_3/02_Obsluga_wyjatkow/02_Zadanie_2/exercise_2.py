cb4d119da081e9b6d1cc4be69c2e169912e4fa89
EwaJakub <ewa.jak@onet.pl>
2023-02-06 18:01:10 +0100

    Obsluga_wyjatkow - exercise_2

--- 05_Dzien_3/02_Obsluga_wyjatkow/02_Zadanie_2/exercise_2.py
+++ 05_Dzien_3/02_Obsluga_wyjatkow/02_Zadanie_2/exercise_2.py
@@ -4,10 +4,17 @@ guessed = False
 rnd = randint(1, 10)
 
 while not guessed:
-    str_num = input("Enter number:")
-    num = int(str_num)
-    if num == rnd:
-        print("Great!")
-        guessed = True
-    else:
-        print("Wrong!")
+    try:
+        str_num = input("Enter number:")
+        num = int(str_num)
+        if num == rnd:
+            print("Great!")
+            guessed = True
+        elif num not in range(1, 11):
+            raise UserWarning
+        else:
+            print("Wrong!")
+    except ValueError:
+        print("Value is incorrect!")
+    except UserWarning:
+        print("Value out of range")
